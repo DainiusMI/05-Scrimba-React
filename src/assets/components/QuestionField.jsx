67ea1725941eba2ba09f2760333a55df10be71ea
DainiusMI <mental.def@gmail.com>
2022-12-13 14:36:25 +0200

    dynamic classes to answers

--- src/assets/components/QuestionField.jsx
+++ src/assets/components/QuestionField.jsx
@@ -2,44 +2,40 @@ import React from "react";
 import { useEffect } from "react";
 
 export default function QuestionField(props) {
+
     function cleanUp(string) {
         return string.replaceAll("&quot;", "\"").replaceAll("&#039;", "\'");
     }
+
     const question = cleanUp(props.data.question);
-    //const answers = props.data.answers
-    let answers =  props.data.answers;
+
+    let answers = props.data.answers;
     useEffect(() => {
         answers = answers.sort((a, b) => 0.5 - Math.random())
-        console.log("effect")
     }, [])
+
+    // rendered answers still placed in order
+
 
     return (
         <div className="question" id={props.id}>
-            <p className="text">{question}</p>
+            <p className="text no-user-select">{question}</p>
             <ul className="answers">
                 {
                     answers.map((answer, idx) => {
                         return (
                             <li 
                                 key={idx} 
-                                className={answer.isHeld ? "selected option" : "option" }                                
-                            >
-                                
-                                <label 
-                                    className="label" 
-                                    htmlFor={`q${props.id}-a${idx}`}
-                                >{cleanUp(answer.value)}</label>
+                                // modify this to check
+                                //className={answer.isHeld ? "selected option no-user-select" : "option no-user-select" }  
+                                className={props.checkAnswers(answer)}  
 
-                                <input 
-                                    className="input"
-                                    type="radio" 
-                                    id={`q${props.id}-a${idx}`} 
-                                    name={props.id}
-                                    value={answer.value}
-                                    onClick={props.handleAnswers}
-                                    //checked={console.log(`q${props.id}-a${idx}`)}
-                                />
-                            </li>
+                                id={`q${props.id}-a${idx}`} 
+                                data-value={answer.value}
+                                data-position={props.id}
+                                onClick={props.handleAnswers}
+                                
+                            >{cleanUp(answer.value)}</li>
                         )
                     })
                 }
--- src/assets/components/QuizScreen.jsx
+++ src/assets/components/QuizScreen.jsx
@@ -9,14 +9,23 @@ export default function QuizScreen() {
         fetch("https://opentdb.com/api.php?amount=5&category=20&difficulty=easy&type=multiple")
             .then(res => res.json())
             //.then(data => setTrivitaData(data.results))
-            .then(data => {
-                setGameData(data.results.map(item => {
-                    return {
-                        question: item.question,
-                        answers: item.incorrect_answers.map(answer => {return {value: answer, isCorrect: false, isHeld: false}}).concat({value: item.correct_answer, isCorrect: true, isHeld: false})
-                    }
-                }))
-            })
+                .then(data => {
+                    setGameData(data.results.map(item => {
+                        return {
+                            question: item.question,
+                            answers: item.incorrect_answers.map(answer => {
+                                return {
+                                    value: answer, 
+                                    isCorrect: false, 
+                                    isHeld: false
+                                }}).concat({
+                                    value: item.correct_answer, 
+                                    isCorrect: true, 
+                                    isHeld: false
+                                })
+                        }
+                    }))
+                })
             /*
             setGameData(triviaData.map(item => {
                 return {
@@ -30,17 +39,63 @@ export default function QuizScreen() {
 
 
     function handleAnswers(event) {
-        const {name, value} = event.target;
+
+        const {value, position} = event.target.dataset;
+
         setGameData(prevState => prevState.map((prevData, idx) => {
-            return idx != name ? prevData : {
+            return idx != position ? prevData : {
                 question: prevData.question,
                 answers: prevData.answers.map(answer => answer.value === value ? { value: answer.value, isHeld: !answer.isHeld, isCorrect: answer.isCorrect } : !answer.isHeld ? answer : { value: answer.value, isHeld: !answer.isHeld, isCorrect: answer.isCorrect })
             }
         }))
     }
 
+    
+    const [gameState, setGameState] = React.useState({
+        runCheck: false,
+        correct: 0
+    })
+    function checkAnswers(answer) {
+                
+        const {isHeld, isCorrect} = answer;
+        let className = ""
+        function select() {
+            className = isHeld ? "select option no-user-select" : "option no-user-select"
+        }
+        function check() {
+            if (isHeld && isCorrect) {
+                className = "option correct"
+                setGameState(prevState => {
+                    return {
+                        ...prevState,
+                        correct: prevState.correct + 1
+                    }
+                })
+            }
+            else if (isHeld) {
+                className = "option incorrect"
+            }
+            else {
+                className = "option"
+            }
+        }
+        gameState.runCheck ? check() : select()
+        return className 
+        // if selected and correct apply correct if not check if it was selected if yes apply inccorect if not just option
+        
+        isHeld && isCorrect ? "option correct" : isHeld ? "option incorrect" : "option"
+    }
+    function runCheck() {
+        setGameState(prevState => {
+            return {
+                ...prevState,
+                runCheck: true
+            }
+        })
+    }
+    gameState.runCheck && console.log("check answers")
     return (
-        <form className="quiz-screen">
+        <section className="quiz-screen">
             {
                 gameData.length && gameData.map((question, idx) => {       
                     return (
@@ -49,7 +104,9 @@ export default function QuizScreen() {
                             id={idx} 
                             data={question}
                             handleAnswers={handleAnswers}
+                            checkAnswers={checkAnswers}
 
+                            setGameData={setGameData}
                         />    
                     )
                 } )
@@ -57,8 +114,8 @@ export default function QuizScreen() {
             }
             <button 
                 className="button"
-                onClick={handleAnswers}
+                onClick={runCheck}
             >Check answers</button>
-        </form>
+        </section>
     )
 }
