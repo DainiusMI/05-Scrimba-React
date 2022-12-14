4c2bf845f8f66845d7155c87e8d2c8a91eac4202
DainiusMI <mental.def@gmail.com>
2022-12-14 15:01:34 +0200

    settings screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,56 @@
 import React from "react";
 import Bubbles from "./assets/components/Bubbles";
 import StartScreen from "./assets/components/StartScreen";
+import QuizSettings from "./assets/components/QuizSettings";
 import QuizScreen from "./assets/components/QuizScreen";
 import "./assets/styles/css/App.css"
 export default function App() {
+  const triviaURL = [
+    {
+      general: {
+        easy:"https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple",
+        medium: "https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple",
+        hard: "https://opentdb.com/api.php?amount=5&category=9&difficulty=hard&type=multiple"
+      },
+      mythology: {
+        easy: "https://opentdb.com/api.php?amount=5&category=20&difficulty=easy&type=multiple",
+        medium: "https://opentdb.com/api.php?amount=5&category=20&difficulty=medium&type=multiple",
+        hard: "https://opentdb.com/api.php?amount=5&category=20&difficulty=hard&type=multiple"
+      },
+      sports: {
+        easy: "https://opentdb.com/api.php?amount=5&category=21&difficulty=easy&type=multiple",
+        medium: "https://opentdb.com/api.php?amount=5&category=21&difficulty=medium&type=multiple",
+        hard: "https://opentdb.com/api.php?amount=5&category=21&difficulty=hard&type=multiple"
+      },
+      geography: {
+        easy: "https://opentdb.com/api.php?amount=5&category=22&difficulty=easy&type=multiple",
+        medium: "https://opentdb.com/api.php?amount=5&category=22&difficulty=medium&type=multiple",
 
+      }
+    }
+  ]
+  const templateURL = {
+    general_knowladge: "https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple",
+    mythology: "https://opentdb.com/api.php?amount=5&category=20&difficulty=easy&type=multiple",
+    sports: "https://opentdb.com/api.php?amount=5&category=21&difficulty=easy&type=multiple",
+    geography: "https://opentdb.com/api.php?amount=5&category=22&difficulty=easy&type=multiple",
+    history: "https://opentdb.com/api.php?amount=5&category=23&difficulty=easy&type=multiple",
+    animals: "https://opentdb.com/api.php?amount=5&category=27&difficulty=easy&type=multiple"
+  }
+
+  const [gemeSettings, setGameSettings] = React.useState({
+    category: "general_knowladge",
+    difficulty: "easy"
+  })
+  console.log(gemeSettings)
   return (
     /*
       you will need to store state for quiz options here
     */
+   
     <main>
-
-        <QuizScreen />
+      
+      <QuizSettings setGameSettings={setGameSettings} templateURL={templateURL}/>
 
     </main>
   )
